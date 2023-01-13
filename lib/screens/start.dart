b529839cc333151f10e0337229d76065207115ad
Jpercast <33771111+Jpercast@users.noreply.github.com>
2023-01-13 12:44:07 -0500

    new!

--- lib/screens/start.dart
+++ lib/screens/start.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 
 late List<Map<String, dynamic>> searchResults;
 
+//Se define una clase que extiende de StatefulWidget y su estado se maneja con _BookSearchPageState
 class BookSearchPage extends StatefulWidget {
   @override
   _BookSearchPageState createState() => _BookSearchPageState();
@@ -21,12 +22,13 @@ class _BookSearchPageState extends State<BookSearchPage> {
     Dio dio = Dio();
     String url =
         'https://www.googleapis.com/books/v1/volumes?q=$query&key=AIzaSyBy5gJHq5VoHENbyhZP4ZiET_ROkdp0OiM';
-
-//En este bloque se hace la petición HTTP utilizando el método get de Dio, especificando la URL de la API de Google Books.
-//En caso de éxito, el resultado es parseado y se devuelve una lista de Map<String, dynamic> que representa los items encontrados.
-//En caso de error se imprime en consola.
     try {
-      final response = await dio.get(url);
+      final response = await dio.get(url).catchError((error) {
+        if (error.response.statusCode == 404) {
+          return ["not_found"];
+        }
+        throw error;
+      });
       return (response.data['items'] as List)
           .map((i) => i as Map<String, dynamic>)
           .toList();
@@ -41,81 +43,95 @@ class _BookSearchPageState extends State<BookSearchPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Busca tu Libro'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              // Código para manejar la acción de cierre de sesión
+              Navigator.pushNamed(context, '/');
+            },
+          ),
+        ],
       ),
       body: Column(
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: (value) {
-                setState(() {
-                  query = value;
-                });
-              },
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    query = value;
+                  });
+                },
+              )),
           MaterialButton(
-              child: Text('Search'),
+              child: Text('Buscar Libro'),
               onPressed: () {
-                setState(() {});
+                if (query != null) {
+                  setState(() {});
+                }
               }),
           Expanded(
             //`FutureBuilder` para mostrar los resultados obtenidos de la petición HTTP.
             //El `FutureBuilder` tiene un `future` que es el resultado del método `getBooks` y un `builder` -
             //que se encarga de mostrar los resultados en una `ListView`.
             child: FutureBuilder(
-              future: getBooks('$query'),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  searchResults = snapshot.data;
-                  return ListView.builder(
-                    itemCount: searchResults.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title:
-                            Text(searchResults[index]['volumeInfo']['title']),
-                        onTap: () {
-                          Navigator.pushNamed(
-                            context,
-                            '/book',
-                            arguments: searchResults[index]['volumeInfo'],
-                          );
+                future: query != null ? getBooks('$query') : null,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data.length == 1 &&
+                        snapshot.data[0] == "not_found") {
+                      return Center(child: Text("No se encontró nada"));
+                    }
+                    searchResults = snapshot.data;
+                    if (searchResults != null && searchResults.isNotEmpty) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: searchResults.length,
+                        scrollDirection: Axis.vertical,
+                        itemBuilder: (BuildContext context, int index) {
+                          //index = searchResults.length;
+                          return ListTile(
+                              leading: Text(searchResults[index]['volumeInfo']
+                                  ['publishedDate']),
+                              title: Text(
+                                  searchResults[index]['volumeInfo']['title']),
+                              onTap: () {
+                                Navigator.pushNamed(context, '/book',
+                                    arguments: searchResults[index]
+                                        ['volumeInfo']);
+                              },
+                              subtitle: Text(searchResults[index]['volumeInfo']
+                                  ['authors'][0]),
+                              trailing: Container(
+                                width: 60.0,
+                                height: 60.0,
+                                child: searchResults[index]['volumeInfo']
+                                                ['imageLinks'] !=
+                                            null &&
+                                        searchResults[index]['volumeInfo']
+                                                ['imageLinks']['thumbnail'] !=
+                                            null
+                                    ? Image.network(searchResults[index]
+                                            ['volumeInfo']['imageLinks']
+                                        ['thumbnail'])
+                                    : Image.asset("assets/default_image.png"),
+                              ));
                         },
-                        subtitle: Text(
-                            searchResults[index]['volumeInfo']['authors'][0]),
-                        trailing: Container(
-                          width: 60.0,
-                          height: 60.0,
-                          child: Image.network(
-                            searchResults[index]['volumeInfo']['imageLinks'] !=
-                                        null &&
-                                    searchResults[index]['volumeInfo']
-                                            ['imageLinks']['thumbnail'] !=
-                                        null
-                                ? searchResults[index]['volumeInfo']
-                                    ['imageLinks']['thumbnail']
-                                : 'https://via.placeholder.com/150',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        leading: Text(searchResults[index]['volumeInfo']
-                                ['publishedDate']
-                            .substring(0, 4)),
                       );
-                    },
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text(
-                      'Nada que Mostrar "Que Aburrido..."',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
-            ),
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text(
+                          'Nada que Mostrar "Que Aburrido..."',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      );
+                    } else {
+                      return CircularProgressIndicator();
+                    }
+                  }
+                  return Container();
+                }),
           ),
         ],
       ),
@@ -124,17 +140,19 @@ class _BookSearchPageState extends State<BookSearchPage> {
 }
 
 //Clase para detalles de libros
-
 class BookDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Se obtiene el argumento pasado al navegar a esta vista
     final Map<String, dynamic> book =
         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     return Scaffold(
       appBar: AppBar(
-        title: Text(book['title']),
+        title:
+            Text(book['title']), //Se muestra el titulo del libro en la Appbar
       ),
       body: SingleChildScrollView(
+        //Se utiliza ScrollView para poder desplazarse por la vista
         child: Column(
           children: <Widget>[
             Container(
@@ -142,15 +160,19 @@ class BookDetailsPage extends StatelessWidget {
                 height: 200,
                 decoration: BoxDecoration(
                     image: DecorationImage(
-                        image: NetworkImage(book['imageLinks'] != null ||
+                        //Se muestra una imagen
+                        image: NetworkImage(book['imageLinks'] != null &&
                                 book['imageLinks']['thumbnail'] != null
                             ? book['imageLinks']['thumbnail']
                             : 'https://via.placeholder.com/150'),
                         fit: BoxFit.cover))),
             Container(
-              margin: EdgeInsets.only(top: 20),
+              margin:
+                  EdgeInsets.only(top: 20), //Se agrega un margen de 20 arriba
             ),
-            Text(book['description'] ?? 'No hay descripción disponible',
+            Text(
+                book['description'] ??
+                    'No hay descripción disponible', //Se muestra la descripción del libro, en caso de no haber, se muestra un mensaje de error
                 style:
                     const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.justify),
@@ -159,7 +181,7 @@ class BookDetailsPage extends StatelessWidget {
             ),
             Text(
                 book['publishedDate'] ??
-                    'No hay fecha de publicación disponible',
+                    'No hay fecha de publicación disponible', //Se muestra la fecha de publicación del libro, en caso de no haber, se muestra un mensaje de error
                 style:
                     const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
           ],
